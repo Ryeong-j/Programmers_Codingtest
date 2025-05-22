56830c1f552d971011c0629e1a5e762eaf685cca
Ryeong-j <babie0511@naver.com>
2025-05-22 16:11:05 +0900

    [level 0] Title: 코드 처리하기, Time: 25.48 ms, Memory: 9.47 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -4,17 +4,17 @@ def solution(code):
     
     for idx in range(0,len(code)):
         if mode ==0:
-            if code[idx] != str(1) and idx % 2 == 0:
+            if code[idx] != '1' and idx % 2 == 0:
                 answer += code[idx]
 
-            elif code[idx] == str(1):
+            elif code[idx] == '1':
                 mode = 1
 
         elif mode ==1:
-            if code[idx] != str(1) and idx % 2 == 1:
+            if code[idx] != '1' and idx % 2 == 1:
                 answer += code[idx]
 
-            elif code[idx] == str(1):
+            elif code[idx] == '1':
                 mode = 0
     
     if answer=="":
