c8ea7ec7be4d5021a1e79348858bc2080740d394
Ryeong-j <babie0511@naver.com>
2025-05-22 16:09:28 +0900

    [level 0] Title: 코드 처리하기, Time: 39.66 ms, Memory: 9.49 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,23 @@
+def solution(code):
+    answer = ''
+    mode = 0
+    
+    for idx in range(0,len(code)):
+        if mode ==0:
+            if code[idx] != str(1) and idx % 2 == 0:
+                answer += code[idx]
+
+            elif code[idx] == str(1):
+                mode = 1
+
+        elif mode ==1:
+            if code[idx] != str(1) and idx % 2 == 1:
+                answer += code[idx]
+
+            elif code[idx] == str(1):
+                mode = 0
+    
+    if answer=="":
+        answer="EMPTY"
+    
+    return answer
