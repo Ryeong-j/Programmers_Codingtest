ee2809d26a75227cc418aca97ac00049985ffd50
Ryeong-j <babie0511@naver.com>
2025-06-05 10:27:59 +0900

    [level 0] Title: 배열 만들기 2, Time: 248.12 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -2,8 +2,8 @@ def solution(l, r):
     answer = []
     
     for i in range(l, r + 1):
-        for ch in str(i):
-            if ch != '0' and ch != '5':
+        for j in str(i):
+            if j != '0' and j != '5':
                 break
         else:
             answer.append(i)
