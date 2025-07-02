9c771d9bdf55fa554a4a532ef59973004e1fee8d
Ryeong-j <babie0511@naver.com>
2025-07-03 01:45:42 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/README.md
+++ 프로그래머스/0/120818. 옷가게 할인 받기/README.md
@@ -1,6 +1,6 @@
 # [level 0] 옷가게 할인 받기 - 120818 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120818) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120818#) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 03일 01:43:07
+2025년 07월 03일 01:45:40
 
 ### 문제 설명
 
--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,9 +1,9 @@
 def solution(price):
-    if 300000>price>=100000:
-        return int(price*0.95)
-    elif 500000>price>=300000:
-        return int(price*0.9)
-    elif price>=500000: 
-        return int(price*0.8)
-    else:
-        return price
+    if price >= 500000:
+        price=price*0.8
+    elif price >= 300000:
+        price=price*0.9
+    elif price >= 100000:
+        price=price*0.95
+    
+    return int(price)
