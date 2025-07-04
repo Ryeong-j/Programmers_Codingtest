015b7cef2bf841538862e089b3fe561744686dfc
Ryeong-j <babie0511@naver.com>
2025-07-04 16:58:45 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -1,18 +1,18 @@
 def solution(chicken):
-    answer = 0
-    a = []
+#     answer = 0
+#     a = []
     
-    while chicken:
-        a.append(chicken % 10)
-        chicken = chicken // 10
-        answer+=chicken
+#     while chicken:
+#         a.append(chicken % 10)
+#         chicken = chicken // 10
+#         answer+=chicken
         
-    if sum(a)>=10:
-        answer+=sum(a)//10
-        if sum(a)%10+sum(a)//10 >= 10:
-            answer+=(sum(a)%10+sum(a)//10)//10
+#     if sum(a)>=10:
+#         answer+=sum(a)//10
+#         if sum(a)%10+sum(a)//10 >= 10:
+#             answer+=(sum(a)%10+sum(a)//10)//10
  
-    return answer
+#     return answer
 
 # 10951
 # 1095    1
@@ -20,3 +20,18 @@ def solution(chicken):
 #   10      9
 #    1      1
 #   1215
+
+    answer = 0
+    a = 0
+    
+    while chicken:
+        a+=chicken % 10
+        chicken = chicken // 10
+        answer+=chicken
+        
+    if a>=10:
+        answer+= a//10
+        if a%10+a//10 >= 10:
+            answer+=(a%10+a//10)//10
+ 
+    return answer
