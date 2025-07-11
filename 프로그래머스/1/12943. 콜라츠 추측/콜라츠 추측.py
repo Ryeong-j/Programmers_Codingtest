777359ac5626ebd09f7631ca2072b47b4aeb2e43
Ryeong-j <babie0511@naver.com>
2025-07-11 16:12:44 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.26 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -13,8 +13,7 @@ def solution(num):
             num=num*3+1
             answer+=1
             
-    if answer>500:
-        return -1
-
+        if answer>500:
+            return -1
     
     return answer
