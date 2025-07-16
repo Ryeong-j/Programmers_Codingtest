c2d44457101bc6c94eae68094657a13e20393321
Ryeong-j <babie0511@naver.com>
2025-07-16 22:53:40 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.03 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,15 +1,16 @@
 def solution(m):
-    m=list(m.split(" "))
-    a=0
-    for i in range(len(m)):
-        print(m[i])
-        if i == 0:
-            a+=int(m[i])
-        elif m[i] == '+':
-            a+=int(m[i+1])
-            print(a)
-        elif m[i] == '-':
-            a-=int(m[i+1])
-            print(a)
+#     m=list(m.split(" "))
+#     a=0
+#     for i in range(len(m)):
+#         print(m[i])
+#         if i == 0:
+#             a+=int(m[i])
+#         elif m[i] == '+':
+#             a+=int(m[i+1])
+#         elif m[i] == '-':
+#             a-=int(m[i+1])
 
-    return a
+#     return a
+
+#eval() 함수 쓰는 문제인듯
+    return eval(m)
