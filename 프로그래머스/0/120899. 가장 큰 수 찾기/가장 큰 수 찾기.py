38e79a15845f02a41e454a772e42b1314da13296
Ryeong-j <babie0511@naver.com>
2025-07-16 22:33:03 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.00 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,7 +1,3 @@
 def solution(array):
-    answer = []
-    
-    answer.append(max(array))
-    answer.append(array.index(max(array)))
-    
-    return answer
+
+    return [max(array), array.index(max(array))]
