b69495cbf7fdea3a9eb09775f9357ac791a102d9
Ryeong-j <babie0511@naver.com>
2025-07-18 00:09:05 +0900

    [level 0] Title: 제곱수 판별하기, Time: 6.69 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -1,4 +1,13 @@
 def solution(n):
-    # 제곱근을 구해서 정수 부분만 남긴 뒤, 제곱했을 때 n과 같으면 제곱수입니다.
-    return 1 if int(n**0.5)**2 == n else 2
+    answer = 0
+    
+    for i in range(1, n//10):
+        if i**2 == n:
+            return 1
 
+    return 2
+            
+    # if answer % 2 == 0:
+    #     return 2
+    # else:
+    #     return 1
