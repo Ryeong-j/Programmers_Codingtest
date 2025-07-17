369671221741d5717c262ae922f89d2b6c036ff5
Ryeong-j <babie0511@naver.com>
2025-07-18 00:37:01 +0900

    [level 0] Title: OX퀴즈, Time: 0.12 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,12 @@
+def solution(quiz):
+    answer = []
+
+    for i in quiz:
+        a=i.split('=')
+
+        if eval(a[0])==int(a[1]):
+            answer.append('O')
+        else:
+            answer.append('X')
+
+    return answer
