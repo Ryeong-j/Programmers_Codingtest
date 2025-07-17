f4d180c0606404bad266c934eca154281695340a
Ryeong-j <babie0511@naver.com>
2025-07-18 00:43:00 +0900

    [level 0] Title: OX퀴즈, Time: 0.13 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -10,3 +10,27 @@ def solution(quiz):
             answer.append('X')
 
     return answer
+
+#eval안쓰는 버전
+# def solution(quiz):
+#     answer = []
+
+#     for i in quiz:
+#         a, op, b, eq, c = i.split() 
+#         a= int(a)
+#         b= int(b)
+#         c= int(c)
+
+
+#         if op == '+':
+#             if a + b == c:
+#                 answer.append('O')
+#             else:
+#                 answer.append('X')
+#         else:
+#             if a - b == c:
+#                 answer.append('O')
+#             else:
+#                 answer.append('X')    
+
+#     return answer
