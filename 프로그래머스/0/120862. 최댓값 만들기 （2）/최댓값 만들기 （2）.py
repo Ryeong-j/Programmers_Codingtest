a26ef00212e59e68eb98e52b28a4f5440b2bb5c5
Ryeong-j <babie0511@naver.com>
2025-07-18 18:40:01 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,9 +1,9 @@
 def solution(n):
     n.sort()
     
-    return max(n[0]*n[1], n[len(n)-2]*n[len(n)-1])
+    return max(n[0]*n[1], n[-2]*n[-1])
     
-    # if n[0]*n[1] > n[len(n)-2]*n[len(n)-1]:
+    # if n[0]*n[1] > n[-2]*n[-1]:
     #     return n[0]*n[1]
     # else:
-    #     return n[len(n)-2]*n[len(n)-1]
+    #     return n[-2]*n[-1]
