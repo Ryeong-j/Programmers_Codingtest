33d4597155f4a5f4cac26d676a85f0c1bfc5b5f3
Ryeong-j <babie0511@naver.com>
2025-08-04 18:51:46 +0900

    [난이도 2] Title: 장마, Time: 0.31 ms, Memory: 70.35 MB -BaekjoonHub

--- goormlevel/194982/1. 장마/장마.py
+++ goormlevel/194982/1. 장마/장마.py
@@ -1,3 +1,33 @@
+# # UTF-8 encoding when using korean
+# N_M = list(map(int, input().split()))
+# N = list(map(int, input().split()))
+# M = []
+# w = [0]*N_M[0]
+# N_3 = []
+# count = 0
+# result = ''
+
+# for i in range(N_M[1]):
+#    M = list(map(int, input().split()))
+#    count += 1
+#    for j in range(M[0]-1,M[1]):
+#       w[j] += 1
+#       N_3.append(j)
+#    if count % 3 == 0:
+#       for k in range(len(w)):
+#          if k in N_3:
+#             w[k] -= 1
+#       N_3 = []
+
+# for i in range(len(w)):
+#    N[i] += w[i]
+
+# for i in N:
+#    result += str(i)
+#    result += ' '
+
+# print(result)
+#==========================================================================================
 n, m = input().split()
 home_num = int(n)
 rain = int(m)
@@ -26,43 +56,12 @@ for _ in range(rain):
 				
 for i in range(len(home)):
     home[i] += abs(water[i])
+	
 print(' '.join(map(str, home)))
+# print(*home) #home이 정수로 이루어진 리스트라서 이렇게 해도 됨_리스트안의 정수 원소들을 문자열로 끄집어내줌
 
+#==========================================================================================
 # print(home)
 # print(rain_home)
 # print(' '.join(str(x + y) for x, y in zip(home, rain_home)))
 
-
-
-
-# print(' '.join(map(str, home)))
-
-
-# n, m = input().split()
-# home_num=int(n)
-# rain=int(m)
-# day = 0
-# rain_home = [0 * home_num]
-
-# home = list(input().split())
-# for i in range(len(home)):
-# 	home[i]= int(home[i])
-
-# for i in range(rain):
-# 	s, e = input().split()
-# 	s= int(s)
-# 	e= int(e)
-
-# 	for j in rain_home[s:e+1]:
-# 		j += 1
-
-# 	day +=1
-
-# 	if day % 3 == 0:
-# 		for k in rain_home:
-# 			if k != 0:
-# 				home[i] += 1
-# 				k = 0
-
-# 	for i, j in zip(rain_home, home):
-# 		print(i + j)
